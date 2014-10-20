721aafd84471c036604e1946c9e5e6a169950e35
Vijay Mahrra <vijay.mahrra@gmail.com>
2014-10-20 17:13:55 +0100

    Aphorism Background Image
    Add a random background image for the aphorism

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ An App which saves, retrieves, edits and displays aphorisms
 
 This app is written in Python using the Kivy library for cross-platform support (Android, IOS, Windows, Linux, Mac OSX).  See http://kivy.org/docs/guide/packaging.html for instructions on packaging the application for the different platforms.
 '''
+import glob
+import os
+import random
+
 import kivy
 kivy.require('1.8.0')
 from kivy.config import Config
-from kivy.app import App
 from kivy.utils import platform
+from kivy.core.window import Window
+from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, ListProperty
 
 from peewee import *
 import models
@@ -50,10 +55,43 @@ class MainWindow(BoxLayout):
     quote_font    = ObjectProperty()
     author_font   = ObjectProperty()
     button_font   = ObjectProperty()
+    backgrounds = ListProperty([])
+    background_image = ObjectProperty()
+
+    #background_image = ObjectProperty()
+
+    def __init__(self,**kwargs):
+        """
+        Initialise app. Load in background images
+        :param kwargs:
+        :return:
+        """
+        super(MainWindow,self).__init__()
+        self.backgrounds.append(self.get_bg_images())
+        self.background_image.source = self.get_rnd_bg_image()
+
+    def get_bg_images(self):
+        """
+        Get a the list of background images
+        :return: list of the background images
+        """
+        for root, dirs, files in os.walk('assets/img/bg'):
+            for file in files:
+                if file.endswith('.jpg'):
+                     self.backgrounds.append(os.path.join(root, file))
+        return self.backgrounds
+
+    def get_rnd_bg_image(self):
+        """
+        Get a random background image
+        :return: path to random background image
+        """
+        return random.choice(self.backgrounds)
 
     def button_random_press(self, *args):
         action = args[0]
         if action == 'random':
+            self.background_image.source = self.get_rnd_bg_image()
             for a in models.Aphorism.select().order_by(fn.Random()).limit(1):
                 self.quote_text.text = self.quote_format.format(
                                             aphorism = a.aphorism,
@@ -75,7 +113,6 @@ class MainApp(App):
 
     def build(self):
         return MainWindow()
-
 
 if __name__ == '__main__':
     MainApp().run()
