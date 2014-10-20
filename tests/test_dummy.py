71d50b00d439b9441d060943fc6676c2ac9dec28
Vijay Mahrra <vijay.mahrra@gmail.com>
2014-10-20 11:39:19 +0100

    added dummy test

---
+++ tests/test_dummy.py
@@ -0,0 +1,12 @@
+import unittest
+
+class XXXTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # import class and prepare everything here.
+        pass
+
+    def test_YYY(self):
+        # place your test case here
+        a = 1
+        self.assertEqual(a, 1)
