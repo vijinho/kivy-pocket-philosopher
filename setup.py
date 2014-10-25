31c1a13e55b0a527ebed6e4a44f05ec448a5c265
Vijay Mahrra <vijay.mahrra@gmail.com>
2014-10-25 10:44:13 +0100

    packaging changes

--- setup.py
+++ setup.py
@@ -6,21 +6,70 @@ except ImportError:
 setup(
     version='1.0',
     name='aforgizmo',
-    description='Pocket Philosopher (Aforgizmo Aphorisms)',
+    description='Pocket Philosopher',
+    long_description='Pocket Philosopher (Aforgizmo Aphorisms) is a tool to store and display aphorisms.',
+    platforms = ['Linux','OSX','Android'],
     author='Vijay Mahrra',
     license='GPL3.0',
+    classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Environment :: Desktop Environment',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License V3.0',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: Google :: Android',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Communications :: Personal'
+          ],
     url='https://github.com/vijinho/aforgizmo',
     download_url='https://github.com/vijinho/aforgizmo',
+    package_dir = {'': 'aforgizmo'},
+    data_files=[
+        ('data', ['data/aphorisms.db', 'data/aphorisms.json']),
+        ('assets', [
+            'assets/fonts/roboto/Apache License.txt',
+            'assets/fonts/roboto/RobotoSlab-Bold.ttf',
+            'assets/fonts/roboto/RobotoSlab-Light.ttf',
+            'assets/fonts/roboto/RobotoSlab-Regular.ttf',
+            'assets/fonts/roboto/RobotoSlab-Thin.ttf',
+            'assets/img/bg/COPYRIGHT.rst',
+            'assets/img/bg/IMGP1149_1.jpg',
+            'assets/img/bg/IMGP1155_1.jpg',
+            'assets/img/bg/IMGP1181_2.jpg',
+            'assets/img/bg/IMGP1221_1.jpg',
+            'assets/img/bg/IMGP1308_1.jpg',
+            'assets/img/bg/IMGP1313_1.jpg',
+            'assets/img/bg/IMGP1324_2.jpg',
+            'assets/img/icon.png',
+            'assets/img/icons/about.png',
+            'assets/img/icons/edit.png',
+            'assets/img/icons/help.png',
+            'assets/img/icons/list.png',
+            'assets/img/icons/new.png',
+            'assets/img/icons/quit.png',
+            'assets/img/icons/search.png',
+            'assets/img/icons/settings.png',
+            'assets/img/icons/warning.png',
+            'assets/img/logo.png']
+        ),
+    ],
     py_modules=['aforgizmo'],
     install_requires=[
-        'Click',
-        'Peewee',
-        'nose'
+        'nose>=1.3.4',
+        'Click>=3.3',
+        'Peewee>=2.3.3',
+        #'kivy>=1.8.0'
     ],
-    packages=['aforgizmo'],
+    packages=[],
     scripts=[],
     entry_points='''
         [console_scripts]
-        aforgizmo=aforgizmo:cli
+        aforgizmo=cli:cli
+        #[gui_scripts]
+        #aforgizmo_gui=main:run
     '''
 )
